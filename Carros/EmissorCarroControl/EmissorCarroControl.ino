8306d7a84f98ff76ddbf6d7b79156365ebcb5557
FBuenoD <fernandobuenodansi@gmail.com>
2018-10-10 21:40:13 -0300

    Car

---
+++ Carros/EmissorCarroControl/EmissorCarroControl.ino
@@ -0,0 +1,74 @@
+#include <IRremote.h>
+
+IRsend irsend;
+
+//O LED IR deve estar conectado ao pino 3 do Arduino PWM.
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void emitirSinal(int opcao) {
+
+  if (opcao == 1) {
+
+    Serial.println("PARAR");
+
+    for (int i = 0; i < 10; i++) {
+      irsend.sendSony(0x1, 32);
+      delay(40);
+    }
+
+  } else if (opcao == 2) {
+
+    Serial.println("FRENTE");
+
+    for (int i = 0; i < 10; i++) {
+      irsend.sendSony(0x2, 32);
+      delay(40);
+    }
+
+  } else if (opcao == 3) {
+
+    Serial.println("TRAZ");
+
+    for (int i = 0; i < 10; i++) {
+      irsend.sendSony(0x3, 32);
+      delay(40);
+    }
+
+  } else if (opcao == 4) {
+
+    Serial.println("ESQUERDA");
+
+    for (int i = 0; i < 10; i++) {
+      irsend.sendSony(0x4, 32);
+      delay(40);
+    }
+
+  } else if (opcao == 5) {
+
+    Serial.println("DIREITA");
+
+    for (int i = 0; i < 10; i++) {
+      irsend.sendSony(0x5, 32);
+      delay(40);
+    }
+
+  }
+
+}
+
+void loop() {
+
+  int opcao;
+
+  if (Serial.available() > 0) {
+    opcao = Serial.parseInt();
+    Serial.println(opcao);
+  }
+
+  emitirSinal(opcao);
+
+  delay(1000);
+}
---
+++ Carros/EsteiraControle/EsteiraControle.ino
@@ -0,0 +1,141 @@
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+
+//motor_A
+int IN1 = 4 ;
+int IN2 = 5 ;
+
+//motor_B
+int IN3 = 6 ;
+int IN4 = 7 ;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+
+void dump(decode_results *results) {
+  // Dumps out the decode_results structure.
+  // Call this after IRrecv::decode()
+  int count = results->rawlen;
+  if (results->decode_type == UNKNOWN) {
+    Serial.print("Unknown encoding: ");
+  }
+  else if (results->decode_type == NEC) {
+    Serial.print("Decoded NEC: ");
+
+  }
+  else if (results->decode_type == SONY) {
+    Serial.print("Decoded SONY: ");
+  }
+  else if (results->decode_type == RC5) {
+    Serial.print("Decoded RC5: ");
+  }
+  else if (results->decode_type == RC6) {
+    Serial.print("Decoded RC6: ");
+  }
+  else if (results->decode_type == PANASONIC) {
+    Serial.print("Decoded PANASONIC - Address: ");
+    Serial.print(results->address, HEX);
+    Serial.print(" Value: ");
+  }
+  else if (results->decode_type == LG) {
+    Serial.print("Decoded LG: ");
+  }
+  else if (results->decode_type == JVC) {
+    Serial.print("Decoded JVC: ");
+  }
+  else if (results->decode_type == AIWA_RC_T501) {
+    Serial.print("Decoded AIWA RC T501: ");
+  }
+  else if (results->decode_type == WHYNTER) {
+    Serial.print("Decoded Whynter: ");
+  }
+  Serial.print(results->value, HEX);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+  Serial.print("Raw (");
+  Serial.print(count, DEC);
+  Serial.print("): ");
+
+  for (int i = 1; i < count; i++) {
+    if (i & 1) {
+      Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
+    }
+    else {
+      Serial.write('-');
+      Serial.print((unsigned long) results->rawbuf[i]*USECPERTICK, DEC);
+    }
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    dump(&results);
+
+    //FF18E7 = 2 = 16718055
+    //FF5AA5 = 6 = 16734885
+    //FF4AB5 = 8 = 16730805
+    //FF10EF = 4 = 16716015
+
+    long int valorRec = results.value;
+    Serial.println(results.value);
+
+    //Porta da Ponte H e Velocidade
+    analogWrite(8, 50);
+    analogWrite(8, 50);
+
+    if (valorRec == 1) {
+      Serial.println("Frente");
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      delay(500);
+    } else if (valorRec == 2) {
+      Serial.println("Frente");
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      delay(500);
+    } else if (valorRec == 3) {
+      Serial.println("Traz");
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      delay(500);
+    } else if (valorRec == 4) {
+      Serial.println("Gira Esquerda");
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      delay(500);
+    } else if (valorRec == 5) {
+      Serial.println("Gira Direita");
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      delay(500);
+    }
+
+    irrecv.resume(); // Receive the next value
+  }
+}
